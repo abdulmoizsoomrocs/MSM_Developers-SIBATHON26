0547d5d4b9e5192c2c48b98e0f71b6e1caea1b34
abdulmoizsoomrocs <abdulmoizsoomrocs@gmail.com>
2026-02-14 11:19:25 +0500

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import SendMoney from "./pages/SendMoney"; 
+import ReceiveMoney from "./pages/ReceiveMoney";
+import PayBills from "./pages/PayBills";
+import MobileTopup from "./pages/MobileTopup";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='bg-red-700 text-4xl'> Ayaz Hussain</h1>
-       
-    </>
-  )
+    <Routes>
+      {/* Default route */}
+      <Route path="/" element={<Login />} />
+
+      {/* Auth routes */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Dashboard */}
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Send Money */}
+      <Route path="/send" element={<SendMoney />} />
+      {/* Receive Money */}
+      <Route path="/receive" element={<ReceiveMoney />} />
+
+      {/* Pay Bills*/}
+      <Route path="/bills" element={<PayBills />} />
+
+      {/* Mobile Topup*/}
+      <Route path="/topup" element={<MobileTopup />} />
+
+
+      {/* Fallback route */}
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
